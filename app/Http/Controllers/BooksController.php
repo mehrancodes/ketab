d21d393a22195fae61b63e459cb678370a259a70
Mehran Rasulian <mehran.rasulian@gmail.com>
2016-09-30 23:42:27 +0330

    Create a new Book

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Book;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
 
 /**
  * Class BooksController
@@ -35,5 +36,18 @@ class BooksController
                 ]
             ], 404);
         }
+    }
+
+    /**
+     * POST /books
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function store(Request $request)
+    {
+        $book = Book::create($request->all());
+        return response()->json(['created' => true], 201, [
+            'Location' => route('books.show', ['id' => $book->id])
+        ]);
     }
 }
